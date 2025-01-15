cc9d6c46082d9ac671ca04dc82aac5c47f8ab4ec
George Bruno <georgebruno.ff@gmail.com>
2025-01-15 12:05:30 -0300

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Beatriz minha filha, eu te amo muito! Se vc também me ama dá uma risadinha</h1>
+    return <h1>Beatriz minha filha, eu te amo muito! Se vc também me ama, dá uma risadinha 😎🖤</h1>
 }
 
 export default Home;
