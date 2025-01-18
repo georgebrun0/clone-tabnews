7a91f98b85c1ec003f3b240f678dc419d1853cb0
George Bruno <georgebruno.ff@gmail.com>
2025-01-18 18:36:28 -0300

    adiciona os scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>Beatriz minha filha, eu te amo muito! Se vc também me ama, dá uma risadinha 😎🖤</h1>
+  return (
+    <h1>
+      Beatriz minha filha, eu te amo muito! Se vc também me ama, dá uma
+      risadinha 😎
+    </h1>
+  );
 }
 
 export default Home;
